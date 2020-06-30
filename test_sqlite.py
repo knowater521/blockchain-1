68b1beedcec6a81c8644fcde7502015de666f0e1
oct-month <oct-month@qq.com>
2020-06-30 20:13:04 +0800

    test_sqlite

---
+++ test_sqlite.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+conn = sqlite3.connect("test.db")
+cus = conn.cursor()
+cus.execute("create table user(id varchar(20) primary key, name varchar(20))")
+cus.execute("insert into user(id, name) values(1, '苏三')")
+print(cus.rowcount)
+cus.close()
+conn.commit()
+conn.close()
